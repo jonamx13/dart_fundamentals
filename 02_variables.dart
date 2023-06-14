845a71cb68d3caac7ee5e1f942b686d2fea3e827
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-06-14 13:10:00 -0600

    Dynamic data type

--- 02_variables.dart
+++ 02_variables.dart
@@ -5,12 +5,20 @@ void main() {
   final List<String> abilities = ['impostor'];
   final sprites = <String>['ditto/front.png', 'ditto/back.png'];
 
+  //dinamyc == null
+  dynamic errorMesaage = 'Hello';
+  errorMesaage = true;
+  errorMesaage = [1, 2, 3, 4, 5, 6];
+  errorMesaage = {1, 2, 3, 4, 5, 6};
+  errorMesaage = () => true;
+  errorMesaage = null;
+
   print("""
   $pokemon
   $hp
   $isAlive
   $abilities
   $sprites
-  
+  $errorMesaage
   """);
 }
