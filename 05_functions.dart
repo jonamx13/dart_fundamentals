75268733aca25505a2328bbe6a69b6a26d261cdf
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-06-15 09:24:15 -0600

    named and position parameters

--- 05_functions.dart
+++ 05_functions.dart
@@ -3,6 +3,8 @@ void main() {
   print(greetArrow());
   print('Sum: ${addTwoNumbers(10, 20)}');
   print('SumArrow: ${addTwoNumsArrow(10, 20)}');
+
+  print(greetPerson(name: 'Jonathan'));
 }
 
 String greetEveryone() {
@@ -17,9 +19,14 @@ int addTwoNumbers(int a, int b) {
 
 int addTwoNumsArrow(a, b) => a + b;
 
-// int add2NumsOptional(int a, [int b = 0]) 
+// int add2NumsOptional(int a, [int b = 0])
 int add2NumsOptional(int a, [int? b]) {
   // b = b ?? 0;
   b ??= 0;
   return a + b;
+}
+
+//Parameters by name
+String greetPerson({ required String name, String message = 'Hello,'}) {
+  return '$message $name';
 }
